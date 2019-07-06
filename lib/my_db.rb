bcd1c05f79053e4ce945da233a530d666c677068
sapana <sapshrestha4@gmail.com>
2019-07-06 19:53:47 -0400

    musiclist,playlist and join tables created and populated with necessary data

--- lib/my_db.rb
+++ lib/my_db.rb
@@ -1,3 +1,4 @@
+require "mysql2"
 class DBtransaction
     
     def connect
@@ -10,6 +11,134 @@ class DBtransaction
         @mysql.close()
     end
 
+    def create_table
+      stmt=@mysql.prepare(" create table musiclist_table( sid int not null auto_increment primary key, SongList varchar(255) ); ")
+     #stmt=@mysql.prepare(" create table playlist_table( pid int not null auto_increment primary key, PlayList varchar(255), foreign key (sid) references musiclist_table(sid) ); ")
+      stmt.execute
+      
+      end
+
+      def create_playlisttable
+        stmt=@mysql.prepare(" create table playlist_table( pid int not null auto_increment primary key, PlayList varchar(255));  ")
+       
+         stmt.execute
+      end
+
+      def create_join_table
+        stmt=@mysql.prepare(" create table join_table( sid int ,pid int ,foreign key (pid) references playlist_table(pid), foreign key (sid) references musiclist_table(sid) ); ")
+      stmt.execute
+      end
+
+
+    
+      
+
+    def insert_data
+       # mysql= Mysql2::Client.new(:host => "localhost", :username => "sapana", :password => "S@p@n@123", :database => "music")
+        puts "inserting data into list table"
+        
+        a=Dir["/home/sapana/BootcampProjects/CRUD_Application/Music_Player_ClI/list-of-music/*"]
+        arr=[]
+        a.each do |item|
+          arr=File.basename(item)
+       
+
+        stmt=@mysql.prepare("insert into musiclist_table(SongList)  values ('#{arr}') ")
+        stmt.execute
+        end
+        
+        puts "done"
+        
+        #mysql.close()
+    end
+
+    def view_data
+        #mysql = Mysql2::Client.new(:host => "localhost", :username => "sapana", :password => "S@p@n@123", :database => "music")
+        result= @mysql.query("select SongList from musiclist_table")
+        result.each do |songs|
+          puts  songs
+         end
+     end
+
+     def update_data
+      result1=@mysql.prepare(" update list1  set fname='sabi', city='pkr' where id=1" )
+      result1.execute
+      end
+
+def musictoplay(mid)
+  result= @mysql.query("select SongList from musiclist_table where sid=#{mid}")
+  return result
 end
-connect
-closedb
+
+
+    def delete_data
+        stmt=@mysql.prepare("delete from musiclist ")
+        stmt.execute
+    end
+
+    
+
+    def insert_into_playlist
+      puts "Enter the name of the playlist"
+      playlist_name=gets.chomp
+      stmt=@mysql.prepare("insert into playlist_table(PlayList)  values ('#{playlist_name}') ")
+      stmt.execute
+      $i=0
+
+      
+      while $i<5 do
+       
+    puts "Select the songs to add"
+    view_data
+    
+    songno=gets.chomp.to_i
+   
+   
+    stmt1= @mysql.query("select sid from musiclist_table where sid=#{songno}")
+    
+    stmt1.each do |x|
+     @v1=x['sid']
+     
+      
+    end
+
+    stmt2= @mysql.query("select pid from playlist_table where PlayList='#{playlist_name}'")
+    stmt2.each do |y|
+     @v2=y['pid']
+    end
+    
+    stmt3=@mysql.prepare("insert into join_table(sid,pid)  values (#{@v1},#{@v2}) ")
+    
+    
+    stmt3.execute
+    $i+=1
+  end
+  end
+
+
+def view_playlist
+  result= @mysql.query("select pid,PlayList from playlist_table")
+  result.each do |songs|
+    puts  songs
+   end
+end
+
+#UPDATES
+def update_playlist(no,rename)
+  stmt=@mysql.prepare("update  playlist_table set PlayList='#{rename}' where pid=#{no} ")
+  stmt.execute
+
+end
+def delplaylist(delno)
+  stmt=@mysql.prepare("delete from playlist_table p join join_table j on p.pid=j.pid  where p.pid=#{delno} ")
+  stmt.execute
+
+end
+
+
+end
+
+
+
+
+
---
+++ lib/playlist_main.rb
@@ -0,0 +1,115 @@
+require_relative "my_db.rb"
+require "pry"
+
+
+$dbObj = DBtransaction.new
+$dbObj.connect
+#$dbObj.create_table
+#$dbObj.insert_data
+#$dbObj.create_playlisttable
+#$dbObj.insert_into_playlist
+#$dbObj.create_join_table
+
+
+
+
+#$dbObj.delete_data
+#dbObj.view_data
+
+#dbObj.update_data
+class PlaylistMain
+def menu
+    puts "1. View songs"
+    puts "2. Play music"
+    puts "3. Shuffle"
+    puts "4. View Playlist"
+    puts "5. Create new Playlist"
+    puts "6. Manage Playlist"
+    puts "7. Manage songs"
+    puts "8. exit"
+    puts "Please choose an option"
+end
+
+def user_selection
+    puts "welcome to music player"
+    menu 
+    
+    loop do
+        user_choice=gets.chomp.to_i
+    case (user_choice)
+    when 1
+       $dbObj.view_data
+       
+    when 2
+        puts "choose music number:"
+        music_id=gets.chomp.to_i
+        musicname=$dbObj.musictoplay(music_id)
+        musicarray=Array.new
+        musicname.each  do |item|
+            musicarray<< item
+            playmusic(musicarray)
+        end
+        
+       
+       # %x(find /home/sapana/repos/bootcamp/music_playlist/list-of-music -type f -name "*.mp3" | mpg123  --control -C --list -)
+            #end
+    when 3
+        %x(mpg123 --control --shuffle -Z ../list-of-music/*)
+    when 4
+       $dbObj.view_playlist
+    when 5
+       $dbObj.insert_into_playlist
+       
+    when 6
+        puts "1.Rename"
+        puts "2.Delete Playlist?"
+        puts "3.Delete Songs from PlayList?"
+        puts  "4.exit"
+        user_input=gets.chomp.to_i
+        case (user_input)
+            when 1
+                puts "select the playlist number to rename"
+                renameno=gets.chomp.to_i
+                
+                puts "Enter the name"
+                rename=gets.chomp
+                $dbObj.update_playlist(renameno,rename)
+            when 2
+                puts "Enter the playlist number to delete"
+                delno=gets.chomp.to_i
+                $dbObj.delplaylist(delno)
+            when 3
+                puts "Enter the song number to delete"
+                songdel=gets.chomp.to_i
+            when 4
+                break
+            end
+        
+        
+
+    when 7
+        puts "seven"
+    when 8
+        exit
+    else
+        puts "invalid command"
+    end
+menu
+end
+
+end
+end
+
+def playmusic(array1)
+   
+    array1.each do |k|
+        v=k['SongList']
+        puts v
+        %x(`mpg123 --control  ../list-of-music/'#{v}'`)   
+        %x(`mpg123 --control --continue ../list-of-music/*`) 
+    end
+end
+
+playlistmain=PlaylistMain.new
+playlistmain.user_selection
+$dbObj.closedb
